a3ead6e5aac2cb6c7d3fb9ee8ea506374ddad564
DEV FALDU <devfadadu118@gmail.com>
2024-09-02 11:14:45 +0100

    comments removed

--- Program.cs
+++ Program.cs
@@ -5,13 +5,10 @@ app.MapGet("/", () =>
 {
     DateTimeOffset utcTime = DateTimeOffset.UtcNow;
 
-    // Get the IST time zone
     TimeZoneInfo istZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
 
-    // Convert the UTC time to IST
     DateTimeOffset istTime = TimeZoneInfo.ConvertTime(utcTime, istZone);
 
-    // Format the date and time with the day of the week
     string formattedDateTime = istTime.ToString("dddd, dd MMMM yyyy hh:mm:ss tt");
 
     return formattedDateTime;
